9624df100121964a1c68531368dc666b18dd01ca
Tomoka Narui <sweetmelty2512@gmail.com>
2026-03-11 01:08:31 +0900

    ハンバーガーメニューの動き実装。

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,7 +1,16 @@
 "use strict";
 
 const hamburger = document.getElementById("js-hamburger");
+const header = document.getElementById("header");
+const overlay = document.getElementById("js-drawer-overlay"); // 追加
 
 hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("is-active");
+  header.classList.toggle("is-active");
+  overlay.classList.toggle("is-active"); // overlayにもクラスをつける
+});
+
+// 背景クリックで閉じる処理（これがあると親切！）
+overlay.addEventListener("click", () => {
+  header.classList.remove("is-active");
+  overlay.classList.remove("is-active");
 });
