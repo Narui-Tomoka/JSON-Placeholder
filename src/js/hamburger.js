29fc457730e172e30acfaf4a685ec8a6526e21f3
Tomoka Narui <sweetmelty2512@gmail.com>
2026-03-11 23:59:36 +0900

    JavaScriptコードの無駄を排除

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -6,11 +6,11 @@ const overlay = document.getElementById("js-drawer-overlay"); // 追加
 
 hamburger.addEventListener("click", () => {
   header.classList.toggle("is-active");
-  overlay.classList.toggle("is-active"); // overlayにもクラスをつける
 });
 
-// 背景クリックで閉じる処理（これがあると親切！）
-overlay.addEventListener("click", () => {
-  header.classList.remove("is-active");
-  overlay.classList.remove("is-active");
+// 背景クリックで閉じる処理
+overlay.addEventListener("click", (e) => {
+  if (e.target === overlay) {
+    header.classList.remove("is-active");
+  }
 });
