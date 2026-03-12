174e1b7b09b7e0b3c2a94e57048f41b77f62d6d2
Tomoka Narui <sweetmelty2512@gmail.com>
2026-03-13 00:18:37 +0900

    API通信ととの結果をコンソールに表示。フォームに挿入。

--- src/js/detail.js
+++ src/js/detail.js
@@ -5,13 +5,47 @@
 // ④ 更新処理（PUT / PATCH）
 // ⑤ 削除処理（DELETE）
 
-// まずはページを開いたときにURLからidを取得してコンソールで確認。
+// ページを開いたときにURLからidを取得してコンソールで確認。
 
 const params = new URLSearchParams(window.location.search);
 const postId = params.get("id");
 
 console.log("取得したID：", postId);
 
+// IDチェックして問題なければfetchPost()実行してAPI通信
 if (!postId || isNaN(Number(postId))) {
   console.log("IDが不正です");
+} else {
+  fetchPost();
+}
+
+// API通信で投稿データを取得してコンソールに表示
+async function fetchPost() {
+  try {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${postId}`,
+    );
+
+    if (!response.ok) {
+      throw new Error("投稿の取得に失敗しました");
+    }
+
+    const data = await response.json();
+
+    console.log("取得した投稿：", data);
+
+    // ここからフォームの要素取得
+    const postIdEl = document.getElementById("postId");
+    const userIdEl = document.getElementById("userId");
+    const titleEl = document.getElementById("title");
+    const bodyEl = document.getElementById("body");
+
+    // データをフォームにセット
+    postIdEl.textContent = data.id;
+    userIdEl.value = data.userId;
+    titleEl.value = data.title;
+    bodyEl.value = data.body;
+  } catch (error) {
+    console.error(error);
+  }
 }
